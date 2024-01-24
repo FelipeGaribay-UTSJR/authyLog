d4a8a13643853d1c3ecc162dbaf68c1db7ef9bf8
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-01-23 22:20:29 -0600

    Otra :(

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js"
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore-js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDoYXrbEdBLf9evXOUKb2JoPHbUncvZUAE",
@@ -14,4 +15,4 @@ const firebaseConfig = {
   // Initialize Firebase
   export const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
-   export const db = getFirestore(app)
+   //export const db = getFirestore(app)
