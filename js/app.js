0b1415b51a92eb3dabe5cbd9b3f4297323e8c131
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-01-23 22:03:30 -0600

    Otro Commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import  { auth } from './firebase.js'
+
+// function loginUser() {
+
+const loginForm = 
+          document.querySelector("#formLogin");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = loginForm["correo"].value;
+  const password = loginForm["contrasena"].value;
+
+  console.log(`Correo: ${email}, contraseña: ${password}`);
+
+  try{
+    const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+    console.log("Bienvenido " + userCredentials.user.email)
+  }
+  catch(error){
+    console.log(error.code)
+    if(error.code === 'auth/invalid-login-credentials'){
+      console.log("Datos incorrectos")
+    }
+  }
+})
+
+  // var email = document.getElementById("correo").value;
+  // var password = document.getElementById("contrasena").value;
+
+
+  // console.log(`Correo: ${email}, contraseña: ${password}`);
+
+  // auth.signInWithEmailAndPassword(email, password)
+  //   .then((success) => {
+  //     console.log('Succesfully signed in')
+  //   }).catch((error) => {
+  //     console.log('Error')
+  //   });
+// }
+
